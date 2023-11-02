966195e43c0d8bfc47cfff216967a6d32e769ffd
yangjunyjy <145221682+yangjunyjy@users.noreply.github.com>
2023-11-02 20:49:47 +0800

    Add files via upload

---
+++ Cart.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcMovie.Models
+{
+    public class Cart
+    {
+        public int Id { get; set; } //购物车id
+        [Display(Name = "商品id")]
+        public int product_id { get; set; }//商品id
+
+        [Display(Name = "商品数量")]
+        public int product_num{ get; set; } //商品数量
+
+        [Display(Name = "商品价格")]
+        [DataType(DataType.Currency)]
+        public decimal product_price { get; set; }//商品价格
+
+        [Display(Name ="用户id")]
+        public int user_id { get; set; } //用户id
+        [Display(Name = "创建时间")]
+        [DataType(DataType.Date)]
+        public DateTime createtime { get; set; } //创建时间
+       
+
+    }
+}
---
+++ CartsController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Data;
+using MvcMovie.Migrations;
+using MvcMovie.Models;
+
+namespace MvcMovie.Controllers
+{
+    public class CartsController : Controller
+    {
+        private readonly MvcMovieContext _context;
+
+        public CartsController(MvcMovieContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Carts
+        public async Task<IActionResult> Index()
+        {
+            return _context.Cart != null ?
+                        View(await _context.Cart.ToListAsync()) :
+                        Problem("Entity set 'MvcMovieContext.Cart'  is null.");
+
+        }
+        public async Task<IActionResult> Test1(int searchid)
+        {
+            
+           
+           
+            var carts1 = from m in _context.Cart
+                         where m.product_id == searchid
+                         select m;
+            return View(await carts1.ToListAsync());
+
+        }
+        public async Task<IActionResult> Test2(double? low, double? high)
+        {
+            if (low == null) { low = 0.0; }
+            if (high == null) { high = 999.0; }
+
+            var carts2 = from c in _context.Cart
+                         where
+                         Convert.ToDecimal(low) <= c.product_price
+                         && c.product_price <= Convert.ToDecimal(high)
+                         select c;
+            return View(await carts2.ToListAsync());
+        }
+        // GET: Carts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Cart == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // GET: Carts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,product_id,product_num,product_price,user_id,createtime")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cart);
+        }
+
+        // GET: Carts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Cart == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            return View(cart);
+        }
+
+        // POST: Carts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,product_id,product_num,product_price,user_id,createtime")] Cart cart)
+        {
+            if (id != cart.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartExists(cart.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cart);
+        }
+
+        // GET: Carts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Cart == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // POST: Carts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Cart == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Cart'  is null.");
+            }
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart != null)
+            {
+                _context.Cart.Remove(cart);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartExists(int id)
+        {
+          return (_context.Cart?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
